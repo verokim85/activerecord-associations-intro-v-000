6b02a5cc81aa79ce5a0dc7e73eb1166c142570aa
Veronica Kim <vero.a.kim@gmail.com>
2018-05-09 02:12:16 +0000

    automatically backed up by learn

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,9 +1,9 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
-  def change 
+  def change
     create_table :songs do |t|
       t.string :name
       t.integer :artist_id
       t.integer :genre_id
-    end 
-  end 
+    end
+  end
 end
